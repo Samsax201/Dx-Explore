5a2a4f56d3c29a2617f5f8fe6ac618ce2503d97a
Samsax201 <oluwasanmisamuel001@gmail.com>
2022-08-28 16:31:03 +0100

    new dev

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+var hamb = document.querySelector('.menubars')
+var moblielinks = document.querySelector('.mobile-link')
+var backdrop = document.querySelector('.backdrop')
+function addclass(){
+    hamb.classList.toggle('show')
+    moblielinks.classList.toggle('showlinks')
+    backdrop.classList.toggle('showlinks')
+}
